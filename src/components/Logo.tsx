7b50a67705899a5f431a6173cee258bc0e30e63f
Jeet Majumder <105539066+jeet858@users.noreply.github.com>
2025-07-07 10:02:42 +0530

    Revert "Jeet"

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
-import { logo_big } from "../../public/images";
+import { logo } from "../../public/images";
 
 // components/Header.tsx
 export default function Logo() {
   return (
     <div className="text-center text-white mt-12 space-y-4 flex flex-col items-center">
-      <Image src={logo_big} alt="Logo" className="max-w-[432px] max-h-28" />
+      <Image src={logo} alt="Logo" className="w-24 h-36" />
+      <h1 className="text-4xl font-bold">Welcome to Lawgistic.ai</h1>
     </div>
   );
 }
